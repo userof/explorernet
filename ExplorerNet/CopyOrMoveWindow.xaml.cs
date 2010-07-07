5f3d583ff4d486bdc9f430e794c14f1e8c68ac90
Roman <user.of.services@gmail.com>
2010-07-07 22:33:09 +0300

    ?????? ??????

---
+++ ExplorerNet/CopyOrMoveWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ExplorerNet
+{
+    /// <summary>
+    /// Interaction logic for CopyOrMoveWindow.xaml
+    /// </summary>
+    public partial class CopyOrMoveWindow : Window
+    {
+        public CopyOrMoveWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
